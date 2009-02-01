23aeef22e9c1d47c4e2621e22f57e4657e5353f2
Daniel Johnson <ComputerDruid@gmail.com>
2009-02-01 15:26:34 -0500

    cleaned up the code to be more modular

--- color2grayscale.py
+++ color2grayscale.py
@@ -1,20 +1,26 @@
-file=open("italy.ppm").read().split("\n")[:-1]
-#first line=P3
-#second line=comment
-xyarray=file[2].split(" ")
-count=0
-outarray=[]
-temppixel=[]
-for i in file[3:]:
-	if count==3:
-		count=0
-		outarray.append(int(.3*temppixel[0]+.59*temppixel[1]+.11*temppixel[2]))
-		temppixel=[]
-	temppixel.append(int(i))
-	count+=1
-print "P2"
-print "#written by an awesome (lame) ai class program"
-print file[2]
-print "255"
-for i in outarray:
-	print i
+def color2grayscale(image):
+	#first line=P3
+	#second line=comment
+	xyarray=file[2].split(" ")
+	count=0
+	outarray=[]
+	temppixel=[]
+	outarray.append("P2") #grayscale
+	outarray.append("#written by an awesome (lame) ai class program")#comment
+	outarray.append(file[2]) #dimensions
+	outarray.append(file[3]) #depth
+
+	for i in file[3:]:
+		if count==3:
+			count=0
+			outarray.append(int(.3*temppixel[0]+.59*temppixel[1]+.11*temppixel[2]))
+			temppixel=[]
+		temppixel.append(int(i))
+		count+=1
+	return outarray
+
+if __name__ == "__main__":
+	file=open("italy.ppm").read().split("\n")[:-1]
+	outarray=color2grayscale(file)
+	for i in outarray:
+		print i
