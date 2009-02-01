167eaa79dea215404d98dd57b7be9001a7fd80fd
Daniel Johnson <ComputerDruid@gmail.com>
2009-02-01 15:44:52 -0500

    cleaned up color2grayscale to use input and output files

--- color2grayscale.py
+++ color2grayscale.py
@@ -1,16 +1,17 @@
+#!/usr/bin/python
 def color2grayscale(image):
 	#first line=P3
 	#second line=comment
-	xyarray=file[2].split(" ")
+	xyarray=image[2].split(" ")
 	count=0
 	outarray=[]
 	temppixel=[]
 	outarray.append("P2") #grayscale
 	outarray.append("#written by an awesome (lame) ai class program")#comment
-	outarray.append(file[2]) #dimensions
-	outarray.append(file[3]) #depth
+	outarray.append(image[2]) #dimensions
+	outarray.append(image[3]) #depth
 
-	for i in file[3:]:
+	for i in image[3:]:
 		if count==3:
 			count=0
 			outarray.append(int(.3*temppixel[0]+.59*temppixel[1]+.11*temppixel[2]))
@@ -20,7 +21,25 @@ def color2grayscale(image):
 	return outarray
 
 if __name__ == "__main__":
-	file=open("italy.ppm").read().split("\n")[:-1]
-	outarray=color2grayscale(file)
+	import sys
+	ifilename="italy.ppm"
+	for opts in sys.argv:
+		if opts == "--help" or opts== "-h":
+			print "syntax: color2grayscale [inputfile]"
+			quit()
+	if len(sys.argv)>1:
+		ifilename=sys.argv[1]
+	strippedfilename=ifilename
+	if ifilename[-4:]==".ppm":
+		strippedfilename=ifilename[:-4]
+	ofilename=strippedfilename+".pgm"
+	infile=open(ifilename)
+	inlines=infile.read().split("\n")[:-1]
+	outarray=color2grayscale(inlines)
+	infile.close()
+
+	print "DEBUG: writing to file "+ofilename
+	outfile=open(ofilename,'w')
 	for i in outarray:
-		print i
+		outfile.write(str(i)+"\n")
+	outfile.close()
